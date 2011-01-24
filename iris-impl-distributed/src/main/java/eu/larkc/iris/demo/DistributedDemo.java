626b35ca568858fc356f44cadb48f689a8369306
Florian Fischer <florian.fischer@fogland.org>
2011-01-24 01:03:43 +0100

    Configuration

---
+++ iris-impl-distributed/src/main/java/eu/larkc/iris/demo/DistributedDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eu.larkc.iris.demo;
+
+/**
+ * @author Florian Fischer, fisf
+ *
+ */
+public class DistributedDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
