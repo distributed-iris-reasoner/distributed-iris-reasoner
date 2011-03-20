103df26946e9279ab610cd2ee101c49c119bdee4
Florian Fischer <florian.fischer@fogland.org>
2011-03-20 19:39:16 +0100

    testcase

---
+++ iris-impl-distributed/src/test/java/eu/larkc/iris/evaluator/DistributedNaiveEvaluatorTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package eu.larkc.iris.evaluator;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Florian Fischer
+ *
+ */
+public class DistributedNaiveEvaluatorTest extends TestCase {
+
+	//TODO: complete
+}
